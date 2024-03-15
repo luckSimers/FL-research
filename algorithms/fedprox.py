c6df02b9454856273883e31bc6d4e427cba31dea
Hongquan Liu <lhq2@gmail.com>
2024-03-15 09:03:28 +0000

    add fedprox

---
+++ algorithms/fedprox.py
@@ -0,0 +1,50 @@
+# This file contains the implementation of FedAvg algorithm
+import copy
+import time
+import logging
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from utils import *
+from algorithms import ClientBase, ServerBase
+
+
+class FedProx(ServerBase):
+    def __init__(self, args) -> None:
+        super(FedProx, self).__init__(args, Client)
+    
+    @staticmethod
+    def get_argument():
+        return [
+            Special_Argument('--mu', type=float, default=0.1, help='weight of proximal term'),
+
+        ]
+
+class Client(ClientBase):
+    def __init__(self, args, id, trainset) -> None:
+        super(Client, self).__init__(args, id, trainset)
+        self.clip_grad = args.clip_grad
+        self.loss = nn.CrossEntropyLoss()
+        self.mu = args.mu
+
+
+    def train(self, round_idx, lr, state_dict):
+        st = time.time()
+        self.prepare(lr, state_dict)
+        loader = DataLoader(self.trainset, batch_size=self.batch_size, shuffle=True)
+        anchor = copy.deepcopy(self.model).to(self.device)
+        self.model.train(True)
+        for step in range(self.local_steps):
+            for x, y in loader:
+                x, y = x.to(self.device), y.to(self.device)
+                logits = self.model(x)
+                loss = self.loss(logits, y)
+                self.optimizer.zero_grad()
+                loss.backward()
+                for p, anchor_p in zip(self.model.parameters(), anchor.parameters()):
+                    p.grad += self.mu * (p.data - anchor_p.data.detach())
+                if self.clip_grad > 0:
+                    nn.utils.clip_grad_norm_(self.model.parameters(), self.clip_grad)
+                self.optimizer.step()
+        self.model.to('cpu')
+        logging.info(f'C{self.id:<2d}>> training cost {(time.time() - st)/60:.2f} min')
